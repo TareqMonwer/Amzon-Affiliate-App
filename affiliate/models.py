5b8c23a231237afb8c6f35da50d7c4a4e99a11e0
tareqmonwer <tareqmonwer137@gmail.com>
2020-07-18 15:14:23 +0600

    Customizer abstract model implemented

--- affiliate/models.py
+++ affiliate/models.py
@@ -2,7 +2,30 @@ from django.db import models
 from model_utils.models import TimeStampedModel
 
 
-class Deal(TimeStampedModel):
+class Customizer(TimeStampedModel):
+    IMAGE_SIZE_CHOICE = (
+        ('L', 'Large'),
+        ('M', 'Medium'),
+        ('S', 'Small')
+    )
+    image_size = models.CharField(
+                 max_length=1,
+                 choices=IMAGE_SIZE_CHOICE,
+                 default='L', blank=True)
+    title_length = models.IntegerField(default=-1, blank=True)
+    details_length = models.IntegerField(default=-1, blank=True)
+    
+    class Meta:
+        abstract = True
+    
+    def get_title(self):
+        if self.title_length < 0:
+            return self.product_title
+        elif self.title_length >= 0:
+            return self.product_title[:self.title_length] + '...'
+
+
+class Deal(Customizer):
     product_title = models.CharField(max_length=255)
     product_image_link = models.TextField()
     product_shortlink = models.URLField()
